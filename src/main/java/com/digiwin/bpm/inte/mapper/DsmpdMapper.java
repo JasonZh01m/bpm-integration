eca90e429688d3a488bded0b1d08926d34cafeaa
zhouxh <xinghuai_zhou@163.com>
2017-06-06 16:01:58 +0800

    补上几个文件

---
+++ src/main/java/com/digiwin/bpm/inte/mapper/DsmpdMapper.java
@@ -0,0 +1,23 @@
+package com.digiwin.bpm.inte.mapper;
+
+import com.digiwin.bpm.inte.model.Dsmpd;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public interface DsmpdMapper {
+    int deleteByPrimaryKey(String dsmpd001);
+
+    int insert(Dsmpd record);
+
+    int insertSelective(Dsmpd record);
+
+    Dsmpd selectByPrimaryKey(String dsmpd001);
+
+    int updateByPrimaryKeySelective(Dsmpd record);
+
+    int updateByPrimaryKey(Dsmpd record);
+
+    List<Dsmpd> selectAllDsmpd(Map<String, String> map);
+}
---
+++ src/main/java/com/digiwin/bpm/inte/model/Dsmpd.java
@@ -0,0 +1,336 @@
+package com.digiwin.bpm.inte.model;
+
+/**
+ * 厂商资质
+ */
+public class Dsmpd {
+    /**
+     * OID
+     */
+    private String dsmpd001;
+
+    /**
+     * 资质证书名称
+     */
+    private String dsmpd002;
+
+    /**
+     * 厂商品牌ID
+     */
+    private String dsmpd003;
+
+    /**
+     * 厂商品牌（名称）
+     */
+    private String dsmpd003c;
+
+    /**
+     * ITS协议签约法人体
+     */
+    private String dsmpd004;
+
+    /**
+     * 关联的厂商协议
+     */
+    private String dsmpd005;
+
+    /**
+     * 证书有效期起始日期
+     */
+    private String dsmpd006;
+
+    /**
+     * 证书有效期终止日期
+     */
+    private String dsmpd007;
+
+    /**
+     * 资质证书有效期
+     */
+    private String dsmpd008;
+
+    /**
+     * 资质初始获得时间
+     */
+    private String dsmpd009;
+
+    /**
+     * 是否提醒
+     */
+    private String dsmpd010;
+
+    /**
+     * 邮件通知群组名称
+     */
+    private String dsmpd011;
+
+    /**
+     *  资质适用范畴
+     */
+    private String dsmpd012;
+
+    /**
+     * null
+     */
+    private String dsmpd012c;
+
+    /**
+     * 授权使用区域
+     */
+    private String dsmpd013;
+
+    /**
+     * 授权使用区域
+     */
+    private String dsmpd013c;
+
+    /**
+     * 是否有资质原件
+     */
+    private String dsmpd014;
+
+    /**
+     * 原件存放信息
+     */
+    private String dsmpd015;
+
+    /**
+     * 扫描件存放信息
+     */
+    private String dsmpd016;
+
+    /**
+     * 资质放机要事时间
+     */
+    private String dsmpd017;
+
+    /**
+     * null
+     */
+    private String dsmpd018;
+
+    /**
+     * null
+     */
+    private String dsmpd019;
+
+    public String getDsmpd001() {
+        return dsmpd001;
+    }
+
+    public void setDsmpd001(String dsmpd001) {
+        this.dsmpd001 = dsmpd001 == null ? null : dsmpd001.trim();
+    }
+
+    public String getDsmpd002() {
+        return dsmpd002;
+    }
+
+    public void setDsmpd002(String dsmpd002) {
+        this.dsmpd002 = dsmpd002 == null ? null : dsmpd002.trim();
+    }
+
+    public String getDsmpd003() {
+        return dsmpd003;
+    }
+
+    public void setDsmpd003(String dsmpd003) {
+        this.dsmpd003 = dsmpd003 == null ? null : dsmpd003.trim();
+    }
+
+    public String getDsmpd003c() {
+        return dsmpd003c;
+    }
+
+    public void setDsmpd003c(String dsmpd003c) {
+        this.dsmpd003c = dsmpd003c == null ? null : dsmpd003c.trim();
+    }
+
+    public String getDsmpd004() {
+        return dsmpd004;
+    }
+
+    public void setDsmpd004(String dsmpd004) {
+        this.dsmpd004 = dsmpd004 == null ? null : dsmpd004.trim();
+    }
+
+    public String getDsmpd005() {
+        return dsmpd005;
+    }
+
+    public void setDsmpd005(String dsmpd005) {
+        this.dsmpd005 = dsmpd005 == null ? null : dsmpd005.trim();
+    }
+
+    public String getDsmpd006() {
+        return dsmpd006;
+    }
+
+    public void setDsmpd006(String dsmpd006) {
+        this.dsmpd006 = dsmpd006 == null ? null : dsmpd006.trim();
+    }
+
+    public String getDsmpd007() {
+        return dsmpd007;
+    }
+
+    public void setDsmpd007(String dsmpd007) {
+        this.dsmpd007 = dsmpd007 == null ? null : dsmpd007.trim();
+    }
+
+    public String getDsmpd008() {
+        return dsmpd008;
+    }
+
+    public void setDsmpd008(String dsmpd008) {
+        this.dsmpd008 = dsmpd008 == null ? null : dsmpd008.trim();
+    }
+
+    public String getDsmpd009() {
+        return dsmpd009;
+    }
+
+    public void setDsmpd009(String dsmpd009) {
+        this.dsmpd009 = dsmpd009 == null ? null : dsmpd009.trim();
+    }
+
+    public String getDsmpd010() {
+        return dsmpd010;
+    }
+
+    public void setDsmpd010(String dsmpd010) {
+        this.dsmpd010 = dsmpd010 == null ? null : dsmpd010.trim();
+    }
+
+    public String getDsmpd011() {
+        return dsmpd011;
+    }
+
+    public void setDsmpd011(String dsmpd011) {
+        this.dsmpd011 = dsmpd011 == null ? null : dsmpd011.trim();
+    }
+
+    public String getDsmpd012() {
+        return dsmpd012;
+    }
+
+    public void setDsmpd012(String dsmpd012) {
+        this.dsmpd012 = dsmpd012 == null ? null : dsmpd012.trim();
+    }
+
+    public String getDsmpd012c() {
+        return dsmpd012c;
+    }
+
+    public void setDsmpd012c(String dsmpd012c) {
+        this.dsmpd012c = dsmpd012c == null ? null : dsmpd012c.trim();
+    }
+
+    public String getDsmpd013() {
+        return dsmpd013;
+    }
+
+    public void setDsmpd013(String dsmpd013) {
+        this.dsmpd013 = dsmpd013 == null ? null : dsmpd013.trim();
+    }
+
+    public String getDsmpd013c() {
+        return dsmpd013c;
+    }
+
+    public void setDsmpd013c(String dsmpd013c) {
+        this.dsmpd013c = dsmpd013c == null ? null : dsmpd013c.trim();
+    }
+
+    public String getDsmpd014() {
+        return dsmpd014;
+    }
+
+    public void setDsmpd014(String dsmpd014) {
+        this.dsmpd014 = dsmpd014 == null ? null : dsmpd014.trim();
+    }
+
+    public String getDsmpd015() {
+        return dsmpd015;
+    }
+
+    public void setDsmpd015(String dsmpd015) {
+        this.dsmpd015 = dsmpd015 == null ? null : dsmpd015.trim();
+    }
+
+    public String getDsmpd016() {
+        return dsmpd016;
+    }
+
+    public void setDsmpd016(String dsmpd016) {
+        this.dsmpd016 = dsmpd016 == null ? null : dsmpd016.trim();
+    }
+
+    public String getDsmpd017() {
+        return dsmpd017;
+    }
+
+    public void setDsmpd017(String dsmpd017) {
+        this.dsmpd017 = dsmpd017 == null ? null : dsmpd017.trim();
+    }
+
+    public String getDsmpd018() {
+        return dsmpd018;
+    }
+
+    public void setDsmpd018(String dsmpd018) {
+        this.dsmpd018 = dsmpd018 == null ? null : dsmpd018.trim();
+    }
+
+    public String getDsmpd019() {
+        return dsmpd019;
+    }
+
+    public void setDsmpd019(String dsmpd019) {
+        this.dsmpd019 = dsmpd019 == null ? null : dsmpd019.trim();
+    }
+
+    @Override
+    public String toString() {
+        return "Dsmpd{" +
+                "dsmpd001='" + dsmpd001 + '\'' +
+                ", dsmpd002='" + dsmpd002 + '\'' +
+                ", dsmpd003='" + dsmpd003 + '\'' +
+                ", dsmpd003c='" + dsmpd003c + '\'' +
+                ", dsmpd004='" + dsmpd004 + '\'' +
+                ", dsmpd005='" + dsmpd005 + '\'' +
+                ", dsmpd006='" + dsmpd006 + '\'' +
+                ", dsmpd007='" + dsmpd007 + '\'' +
+                ", dsmpd008='" + dsmpd008 + '\'' +
+                ", dsmpd009='" + dsmpd009 + '\'' +
+                ", dsmpd010='" + dsmpd010 + '\'' +
+                ", dsmpd011='" + dsmpd011 + '\'' +
+                ", dsmpd012='" + dsmpd012 + '\'' +
+                ", dsmpd012c='" + dsmpd012c + '\'' +
+                ", dsmpd013='" + dsmpd013 + '\'' +
+                ", dsmpd013c='" + dsmpd013c + '\'' +
+                ", dsmpd014='" + dsmpd014 + '\'' +
+                ", dsmpd015='" + dsmpd015 + '\'' +
+                ", dsmpd016='" + dsmpd016 + '\'' +
+                ", dsmpd017='" + dsmpd017 + '\'' +
+                ", dsmpd018='" + dsmpd018 + '\'' +
+                ", dsmpd019='" + dsmpd019 + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Dsmpd dsmpd = (Dsmpd) o;
+
+        return dsmpd001 != null ? dsmpd001.equals(dsmpd.dsmpd001) : dsmpd.dsmpd001 == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return dsmpd001 != null ? dsmpd001.hashCode() : 0;
+    }
+}
